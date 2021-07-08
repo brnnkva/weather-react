6a9b9d80a2c649403e713ec55c7415f18bc1db57
brnnkva <64192884+brnnkva@users.noreply.github.com>
2021-07-08 19:59:05 +0200

    added the loader

--- src/Hello.js
+++ src/Hello.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import axios from "axios";
+import Loader from "react-loader-spinner";
 
 export default function Hello(props){
 let [temperature, setTemperature]=useState(null)
@@ -15,6 +16,7 @@ let apiKey = "a2448133104335b630f878b5541b3167";
     return ( 
         <div>
         <h4>The temperature in {props.city} is {temperature}°C</h4>
+        <Loader type="TailSpin" color="#00BFFF" height={80} width={80} />
         </div>
         
 
